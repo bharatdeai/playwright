7d5701a1cb395c1435831711d587dce152bedd3f
bharat <bharat.vinit@gmail.com>
2025-08-14 16:47:39 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,17 +1,17 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://magecomp.com/');
-  await page.locator('#maincontent').getByRole('link', { name: 'Mobile OTP Login' }).click();
-  await page.getByRole('textbox', { name: 'Domain URL' }).click();
-  await page.getByRole('textbox', { name: 'Domain URL' }).fill('www.magecomp.com');
-  await page.getByLabel('Magento Edition').selectOption('2112');
-  await page.getByLabel('Addons').selectOption('3561');
-  await page.getByRole('button', { name: 'Buy Now' }).click();
-  await expect(page.getByText('You added Magento 2 Mobile')).toBeVisible();
-  await page.getByRole('heading', { name: 'Apply Discount Code' }).click();
-  await page.getByRole('textbox', { name: 'Enter discount code' }).click();
-  await page.getByRole('textbox', { name: 'Enter discount code' }).fill('magecomp123');
-  await page.getByRole('button', { name: 'Apply Discount' }).click();
-  await expect(page.getByText('The coupon code "magecomp123')).toBeVisible();
-});
+// test('test', async ({ page }) => {
+//   await page.goto('https://magecomp.com/');
+//   await page.locator('#maincontent').getByRole('link', { name: 'Mobile OTP Login' }).click();
+//   await page.getByRole('textbox', { name: 'Domain URL' }).click();
+//   await page.getByRole('textbox', { name: 'Domain URL' }).fill('www.magecomp.com');
+//   await page.getByLabel('Magento Edition').selectOption('2112');
+//   await page.getByLabel('Addons').selectOption('3561');
+//   await page.getByRole('button', { name: 'Buy Now' }).click();
+//   await expect(page.getByText('You added Magento 2 Mobile')).toBeVisible();
+//   await page.getByRole('heading', { name: 'Apply Discount Code' }).click();
+//   await page.getByRole('textbox', { name: 'Enter discount code' }).click();
+//   await page.getByRole('textbox', { name: 'Enter discount code' }).fill('magecomp123');
+//   await page.getByRole('button', { name: 'Apply Discount' }).click();
+//   await expect(page.getByText('The coupon code "magecomp123')).toBeVisible();
+// });
